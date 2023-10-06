764425a6eb50daae9b9656136cc693efcc31a27d
Fess00 <ss_s0l0@mail.ru>
2023-10-06 20:53:56 +0300

    added /start response message

--- telegrambot/Program.cs
+++ telegrambot/Program.cs
@@ -1,9 +1,9 @@
 ﻿using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 var client = new TelegramBotClient("840186999:AAGd3snwB0ONRTxrDYSEIX0fa_rKrQ2Tu60");
 client.StartReceiving(Update, Error);
-
 Console.ReadKey();
 
 static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
@@ -16,10 +16,14 @@ async static Task Update(ITelegramBotClient client, Update update, CancellationT
     Message? message = update.Message;
     if (message!.Text != null)
     {
-        if (message.Text.ToLower() == "/help")
+        switch (message!.Text.ToLower())
         {
-            await client.SendTextMessageAsync(message.Chat.Id, "Пока боту нечего сказать вам :(");
-            return;
+            case "/start":
+                await client.SendTextMessageAsync(message.Chat.Id, "Привет, это бот пока еще без предназначения. Так что можешь включать режим ждуна :)", replyMarkup: new ReplyKeyboardRemove());
+                break;
+            case "/help":
+                await client.SendTextMessageAsync(message.Chat.Id, "Пока боту нечего сказать вам :(", replyMarkup: new ReplyKeyboardRemove());
+                break;
         }
     }
 }
